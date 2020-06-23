a377c0e04459c573dcdbc9e46795a5667da6a49d
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-06-23 09:09:12 +0530

    configured mongodb using mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+//require the library
+const mongoose = require('mongoose');
+
+//connect to the datbase
+mongoose.connect('mongodb://localhost:27017/codeial_developement', {useNewUrlParser: true});
+// acquire the connection to check if it is successful
+const db = mongoose.connection;
+
+// error
+db.on('error', console.error.bind(console, 'Error connecting to mongoDB'));
+
+// up and running then print the message
+db.once('open', () => {
+    console.log('Connected to database :: mongoDB');
+});
+
+module.exports = db;
