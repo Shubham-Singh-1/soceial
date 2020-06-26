3cffeebe989554c41cbc8e2023fd3cae3b57c8bd
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-06-26 08:25:45 +0530

    Authentication using Passport.js setup

---
+++ config/passport-local-statergy.js
@@ -0,0 +1,28 @@
+const passport =  require('passport');
+
+const LocalStatergy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+//authentication using passport
+passport.use(new LocalStatergy({
+        usernameField: 'email'
+    },
+    (email , password , done) => {
+        //find a user and establish the identity
+        User.findOne({email: email} ,(err,user) => {
+            if(err){
+                console.log('Error in finding user --->Passport');
+                return done(err);
+            }
+            
+            if(!user || user.password!=password){
+                console.log('Invalid Username/Password');
+                return done(null , false);
+            }
+
+            return done(null , user);
+        });
+    }
+
+));
