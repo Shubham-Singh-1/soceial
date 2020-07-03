77232decfd896dbb02f3508cd5b0de2dd4752cb4
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-03 17:27:45 +0530

    convert callbacks to async await in home comtoller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,34 +2,28 @@ const Post = require('../models/post');
 const User = require('../models/user');
 
 
-module.exports.home = ((req,res) => {
-    // console.log(req.cookies);
-    // res.cookie('user_id' ,20);
+module.exports.home = (async (req,res) => {
 
-    // Post.find({} , (err,posts) => {
-    //     return res.render('home' , {
-    //         title:"Codeial | Home",
-    //         posts: posts,
-    //     });
-    // });
-
-    //populate the user of each post
-    Post.find({})
-    .populate({path: 'user'})    
-    .populate({
-        path: 'comments',
-        populate : {
-            path: 'user',
-        }
-    })
-    .exec((err,posts) => {
-
-        User.find({} , (err,user) => {
-            return res.render('home' , {
-                title: "Codeial | Home",
-                posts: posts,
-                all_users: user,
-            });
-        })
-    });
+    try{
+        let posts = await Post.find({})
+        .populate({path: 'user'})    
+        .populate({
+            path: 'comments',
+            populate : {
+                path: 'user',
+            }
+        });
+    
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title:"Codeial | Home",
+            posts: posts,
+            all_users: users,
+        });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+   
 });
