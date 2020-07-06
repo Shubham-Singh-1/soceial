70bfb093fbea68074c5a6a33f43280f846031140
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-06 17:17:20 +0530

    add Post via Ajax request,no need to refresh the entire page again and again

--- controllers/posts _controller.js
+++ controllers/posts _controller.js
@@ -1,43 +1,54 @@
-const { post } = require("../routes/users")
-
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create =(async (req,res) => {
+module.exports.create = async function(req, res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
-            user: req.user._id,
+            user: req.user._id
         });
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
 
-        req.flash('success','Post Published!');
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
+        req.flash('error', err);
         return res.redirect('back');
     }
-    catch(err){
-        req.flash('error',err);
-        return res.redirect('back');
-    }
-});
+  
+}
 
-module.exports.destroy = async (req, res) => {
+
+module.exports.destroy = async function(req, res){
+
     try{
-    let post = await Post.findById(req.params.id);
-        // .id means converting the object id into string
+        let post = await Post.findById(req.params.id);
+
         if (post.user == req.user.id){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
 
-            req.flash('success','Post ans assoiated comments deleted!');
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
-        else{
-            req.flash('error','You Cannot delete this Post!');
-            return res.redirect('back');
-        }
-    }
-    catch(err){
-        req.flash('error',err);
+
+    }catch(err){
+        req.flash('error', err);
         return res.redirect('back');
     }
+    
 }
