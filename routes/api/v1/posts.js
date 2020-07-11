9806084e713d42dff69cc05aafd3d55213bcf826
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-11 23:48:50 +0530

    send and delete data using API and Postman

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsApi = require('../../../controllers/api/v1/posts_api');
 
 router.get('/' , postsApi.index);
+router.delete('/:id', postsApi.destroy);
 
 module.exports = router;
