3de7e139c9685db631ad766f77c19dfef5351883
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-24 23:27:39 +0530

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -28,6 +28,6 @@ router.get('/sign-out', usersController.destroySession);
 
 
 router.get('/auth/google',passport.authenticate('google' ,{scope:['profile','email']}));
-router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'},usersController.createSession));
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),usersController.createSession);
 
 module.exports = router;
