c64a1bc53ab7a6a5147d7234346ada99adf48f0f
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-25 15:41:53 +0530

    install and configure nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+let transporter =  nodemailer.createTransport({
+    service:'gmail',
+    host:'smtp.gmail.com',
+    port:587,
+    secure: false,
+    auth: {
+        user:'shubhamsinghnimbus',
+        pass:'77501bhagatsarkaullu',  
+    }
+});
+
+let renderTemplate = (data,relativePath) => {
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        (err,template) => {
+            if(err){
+                console.log('error in rendering template');
+                return;
+            }
+            mailHTML = template;
+        }
+    )
+
+    return mailHTML; 
+}
+
+module.exports = {
+    transporter : transporter,
+    renderTemplate: renderTemplate,
+}
