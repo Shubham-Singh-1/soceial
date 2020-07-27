c04c68a4b8c078c7e209f4c3655cb5ca2eee5e99
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-07-27 13:47:10 +0530

    Kue set and parallel jobs set to decrease load on CPU

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+
 module.exports.create = async function(req, res){
 
     try{
@@ -17,7 +20,15 @@ module.exports.create = async function(req, res){
             post.save();
             
             comment = await comment.populate('user', 'name email').execPopulate();
-            commentsMailer.newComment(comment);
+            // commentsMailer.newComment(comment);
+            let job = queue.create('emails',comment).save(function(err){
+                if(err){
+                    console.log('error in sending to the queue',err);
+                    return;
+                }
+                console.log('job enqueued',job.id);
+            })
+
             if (req.xhr){
                 
     
