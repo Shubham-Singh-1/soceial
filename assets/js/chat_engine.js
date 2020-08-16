95d3730834cafb2f49628107dd8741ad55b99b43
Shubham-Singh-1 <shubhamsinghnimbus@gmail.com>
2020-08-17 03:10:22 +0530

    create socket.io chat room

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,21 +1,34 @@
 class ChatEngine{
-    constructor(chatBoxId,userEmail){
+    constructor(chatBoxId, userEmail){
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-
         this.socket = io.connect('http://localhost:5000');
 
-
-        if(this.userEmail){
+        if (this.userEmail){
             this.connectionHandler();
         }
 
     }
 
+
     connectionHandler(){
-        this.socket.on('connect',function(){
-            console.log('connection estalblished using sockets....!!');
+        let self = this;
+
+        this.socket.on('connect', function(){
+            console.log('connection established using sockets...!');
+
+
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+            self.socket.on('user_joined', function(data){
+                console.log('a user joined!', data);
+            })
+
+
         });
     }
 }
